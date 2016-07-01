7890e4a0e86c62e3fe9077e2b9e59c483589db14
rccoder <rccoder.net@gmail.com>
2016-07-01 16:21:04 +0800

    add getArtistAblums and getAblums & Update README & Update test & replan directory

---
+++ build/component/getAlbums.js
@@ -0,0 +1,32 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.getAlbums = undefined;
+
+var _request = require('request');
+
+var _request2 = _interopRequireDefault(_request);
+
+var _config = require('../config');
+
+var _util = require('../util');
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var getAlbums = function getAlbums(id, callback) {
+  var option = (0, _util.deepCopy)(_config.globalOption);
+  var url = _config.origin + '/api/album/' + id;
+  var method = 'get';
+  Object.assign(option, { url: url, method: method });
+  (0, _request2.default)(option, function (err, res, body) {
+    if (!err && res.statusCode == 200) {
+      var info = JSON.parse(body);
+      callback && callback(JSON.stringify(info, '', 2));
+    } else {
+      console.error(err);
+    }
+  });
+};
+exports.getAlbums = getAlbums;
---
+++ build/component/getArtistAlbums.js
@@ -0,0 +1,35 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.getArtistAlbums = undefined;
+
+var _request = require('request');
+
+var _request2 = _interopRequireDefault(_request);
+
+var _config = require('../config');
+
+var _util = require('../util');
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var getArtistAlbums = function getArtistAlbums(id, callback) {
+  var limit = arguments.length <= 2 || arguments[2] === undefined ? 3 : arguments[2];
+  var offset = arguments.length <= 3 || arguments[3] === undefined ? 0 : arguments[3];
+
+  var option = (0, _util.deepCopy)(_config.globalOption);
+  var url = _config.origin + '/api/artist/albums/' + id + '?offset=' + offset + '&limit=' + limit;
+  var method = 'GET';
+  Object.assign(option, { url: url, method: method });
+  (0, _request2.default)(option, function (err, res, body) {
+    if (!err && res.statusCode == 200) {
+      var info = JSON.parse(body);
+      callback && callback(JSON.stringify(info, '', 2));
+    } else {
+      console.error(err);
+    }
+  });
+};
+exports.getArtistAlbums = getArtistAlbums;
