581f55346f378cada56d08441d6b7bcba0d121ad
liusheng <3245813812@qq.com>
2017-04-25 16:46:49 +0800

    commit code

--- app.js
+++ app.js
@@ -2,6 +2,16 @@ const express = require('express')
 const http = require('http')
 const app = express()
 
+app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Credentials",true);
+    res.header("Access-Control-Allow-Origin", "http://localhost:8080");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+    res.header("X-Powered-By",' 3.2.1')
+    res.header("Content-Type", "application/json;charset=utf-8");
+    next();
+});
+
 //手机登录
 app.use('/login/cellphone', require('./router/loginCellphone'))
 
@@ -13,32 +23,16 @@ app.use('/comment', require('./router/comment'))
 
 // 获取每日推荐歌曲
 app.use('/recommend/songs', require('./router/recommendSongs'))
-
 // 获取每日推荐歌单
 app.use('/recommend/resource', require('./router/recommendResource'))
 
 // 获取歌词
 app.use('/lyric', require('./router/lyric'))
 
-// 获取专辑内容
+// 获取专辑
 app.use('/album', require('./router/album'))
 
-// 获取歌手单曲
-app.use('/artists', require('./router/artists'))
-
-// 获取歌手专辑列表
-app.use('/artist_album', require('./router/artist_album'))
-
-// 歌单（网友精选碟） hot||new http://music.163.com/#/discover/playlist/
-app.use('/top_playlist', require('./router/top_playlist'))
-
-// 新碟上架 http://music.163.com/#/discover/album/
-app.use('/new_albums', require('./router/new_albums'))
-
-// 热门歌手 http://music.163.com/#/discover/artist/
-app.use('/top_artists', require('./router/top_artists'))
-
-// 获取用户歌单
+// 获取歌单
 app.use('/user/playlist', require('./router/userPlaylist'))
 
 // 获取歌单内列表
@@ -50,13 +44,9 @@ app.use('/playlist/tracks', require('./router/playlistTracks'))
 // 获取音乐 url
 app.use('/music/url', require('./router/musicUrl'))
 
-// 搜索
-app.use('/search', require('./router/search'))
+// 搜歌
+app.use('/search', require('.//router/search'))
 
-// 获取音乐详情
-app.use('/music/songDetail', require('./router/songDetail'))
-
-// 不明 api
 app.use('/log/web', require('./router/logWeb'))
 
 // 私人 FM
@@ -73,12 +63,6 @@ app.use("/fm_trash",require("./router/fm_trash"))
 
 //排行榜
 app.use("/top_list",require("./router/top_list"))
-
-//mv
-app.use("/mv",require("./router/mv"))
-
-//play_mv
-app.use("/play_mv",require("./router/play_mv"))
 
 process.on('SIGHUP', () => {
   console.log('server: bye bye')
