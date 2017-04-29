e3c4377b4ced14eed70d40ad690d376ec43461fc
binaryify <binaryify@gmail.com>
2017-04-29 22:30:29 +0800

    improve

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express()
 // 跨域设置
 app.all('*', function (req, res, next) {
   res.header("Access-Control-Allow-Credentials", true)
-  res.header("Access-Control-Allow-Origin", "http://localhost:8080")
+  res.header("Access-Control-Allow-Origin", "*")
   res.header("Access-Control-Allow-Headers", "X-Requested-With")
   res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS")
   res.header("X-Powered-By", ' 3.2.1')
