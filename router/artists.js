e3b0e67a638a9e2c20ef6fcae50c5805631b7fa4
binaryify <zhuangtongfa@qq.com>
2017-05-02 09:55:41 +0800

    修复歌手单曲数据空白问题和文档获取歌手单曲url 描述问题

--- router/artists.js
+++ router/artists.js
@@ -1,19 +1,24 @@
 const express = require("express")
 const router = express()
-const { createRequest } = require("../util/util")
+const { createWebAPIRequest } = require("../util/util")
 
 router.get("/", (req, res) => {
+  const cookie = req.get('Cookie') ? req.get('Cookie') : ''
+  const data = {
+		"csrf_token": ""
+  }
   const id = req.query.id
   const offset = req.query.offset || 0
   const limit = req.query.limit || 50
-  createRequest(`/weapi/v1/artist/${id}?offset=${offset}&limit=${limit}`, 'GET', null)
-    .then(result => {
-      res.setHeader("Content-Type", "application/json")
-      res.send(result)
-    })
-    .catch(err => {
-      res.status(502).send('fetch error')
-    })
+  createWebAPIRequest(
+    'music.163.com',
+    `/weapi/v1/artist/${id}?offset=${offset}&limit=${limit}`,
+    'POST',
+    data,
+    cookie,
+    music_req => res.send(music_req),
+    err => res.status(502).send('fetch error')
+  )
 })
 
 
