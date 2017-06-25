10b8f63ab3ae26f64b8de07e6440d780d845b4bd
root <zhuangtongfa@qq.com>
2017-06-25 11:03:43 +0800

    修复签到接口 #62

--- router/daily_signin.js
+++ router/daily_signin.js
@@ -1,22 +1,22 @@
 // 签到
-const express = require("express")
+const express = require('express')
 const router = express()
-const { createWebAPIRequest } = require("../util/util")
+const { createWebAPIRequest } = require('../util/util')
 
-router.get("/", (req, res) => {
+router.get('/', (req, res) => {
   const cookie = req.get('Cookie') ? req.get('Cookie') : ''
+  let type = req.query.type || 0 //0为安卓端签到 3点经验,1为网页签到,2点经验
   const data = {
-    "csrf_token": ""
+    csrf_token: '',
+    type
   }
   // {'android': {'point': 3, 'code': 200}, 'web': {'point': 2, 'code': 200}}
   // {'android': {'code': -2, 'msg': '重复签到'}, 'web': {'code': -2, 'msg': '重复签到'}}
   // 'android': {'code': 301}, 'web': {'code': 301}}
 
-  let type = req.query.type || 0  //0为安卓端签到 3点经验,1为网页签到,2点经验
-  const action = `/weapi/point/dailyTask?type=${type}`
   createWebAPIRequest(
     'music.163.com',
-    action,
+    '/weapi/point/dailyTask',
     'POST',
     data,
     cookie,
@@ -24,7 +24,5 @@ router.get("/", (req, res) => {
     err => res.status(502).send('fetch error')
   )
 })
-
-
 
 module.exports = router
