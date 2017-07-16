eab5bf124a1c0cb166ee918493ac2e289bb5dc1b
zhuangtongfa <zhuangtongfa@qq.com>
2017-07-16 18:01:39 +0800

    修复垃圾桶接口问题 #75

--- router/fm_trash.js
+++ router/fm_trash.js
@@ -1,18 +1,20 @@
-const express = require("express")
+const express = require('express')
 const router = express()
-const { createWebAPIRequest } = require("../util/util")
+const { createWebAPIRequest } = require('../util/util')
 
-router.get("/", (req, res) => {
+router.get('/', (req, res) => {
   const cookie = req.get('Cookie') ? req.get('Cookie') : ''
-  const data = {
-    "csrf_token": ""
-  }
-  const songid = req.query.id
-  const alg = "RT"
+  const songId = req.query.id
+  const alg = 'RT'
   const time = req.query.time || 25
+  const data = {
+    csrf_token: '',
+    songId
+  }
+
   createWebAPIRequest(
     'music.163.com',
-    `/weapi/radio/trash/add?alg=${alg}&songId=${songid}&time=${time}`,
+    `/weapi/radio/trash/add?alg=${alg}&songId=${songId}&time=${time}`,
     'POST',
     data,
     cookie,
@@ -20,7 +22,5 @@ router.get("/", (req, res) => {
     err => res.status(502).send('fetch error')
   )
 })
-
-
 
 module.exports = router
