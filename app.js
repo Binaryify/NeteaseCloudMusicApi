2b674a85871171b407a39835223ca814647edb97
zhuangtongfa <zhuangtongfa@qq.com>
2017-08-17 11:09:38 +0800

    优化 cors 设置

--- app.js
+++ app.js
@@ -7,12 +7,14 @@ let cache = apicache.middleware
 
 // 跨域设置
 // app.all('*', function (req, res, next) {
-//   res.header("Access-Control-Allow-Credentials", true)
-//   res.header("Access-Control-Allow-Origin", "*")
-//   res.header("Access-Control-Allow-Headers", "X-Requested-With")
-//   res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS")
-//   res.header("X-Powered-By", ' 3.2.1')
-//   res.header("Content-Type", "application/json;charset=utf-8")
+//   if (req.path !== '/' && !req.path.includes('.')) {
+//     res.header('Access-Control-Allow-Credentials', true)
+//     // 这里获取 origin 请求头 而不是用 *
+//     res.header('Access-Control-Allow-Origin', req.headers['origin'] || '*')
+//     res.header('Access-Control-Allow-Headers', 'X-Requested-With')
+//     res.header('Access-Control-Allow-Methods', 'PUT,POST,GET,DELETE,OPTIONS')
+//     res.header('Content-Type', 'application/json;charset=utf-8')
+//   }
 //   next()
 // })
 
