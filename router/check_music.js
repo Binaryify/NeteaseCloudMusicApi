64f781ee1f0a6438b07e24a39eb925f11e226db5
binaryify <binaryify@gmail.com>
2017-08-17 12:05:03 +0800

    加入 Prettier

--- router/check_music.js
+++ router/check_music.js
@@ -1,14 +1,14 @@
-const express = require("express")
+const express = require('express')
 const router = express()
-const request = require("request")
-const { createWebAPIRequest } = require("../util/util")
-router.get("/", (req, res) => {
-    const id = parseInt(req.query.id)
+const request = require('request')
+const { createWebAPIRequest } = require('../util/util')
+router.get('/', (req, res) => {
+  const id = parseInt(req.query.id)
   const br = parseInt(req.query.br || 999000)
   const data = {
-    "ids": [id],
-    "br": br,
-    "csrf_token": ""
+    ids: [id],
+    br: br,
+    csrf_token: ''
   }
   const cookie = req.get('Cookie') ? req.get('Cookie') : ''
 
@@ -19,16 +19,15 @@ router.get("/", (req, res) => {
     data,
     cookie,
     music_req => {
-      if(JSON.parse(music_req).code==200){
-          return res.send({success: true, message: 'ok'})
+      if (JSON.parse(music_req).code == 200) {
+        return res.send({ success: true, message: 'ok' })
       }
-      return res.send({success: false, message: '亲爱的,暂无版权'});
+      return res.send({ success: false, message: '亲爱的,暂无版权' })
     },
     err => {
       res.status(502).send('fetch error')
     }
   )
 })
-
 
 module.exports = router
