8f74ac1b1992faa714efc501bc40bab29f462d12
binaryify <binaryify@gmail.com>
2017-09-07 17:11:24 +0800

    修复搜索接口 offset 参数失效问题 #100

--- router/search.js
+++ router/search.js
@@ -1,30 +1,31 @@
-const express = require('express')
-const router = express()
-const { createWebAPIRequest } = require('../util/util')
+const express = require("express");
+const router = express();
+const { createWebAPIRequest } = require("../util/util");
 
-router.get('/', (req, res) => {
-  const cookie = req.get('Cookie') ? req.get('Cookie') : ''
-  const keywords = req.query.keywords
-  const type = req.query.type || 1
-  const limit = req.query.limit || 30
-  const offset = req.query.offset || 0
+router.get("/", (req, res) => {
+  const cookie = req.get("Cookie") ? req.get("Cookie") : "";
+  const keywords = req.query.keywords;
+  const type = req.query.type || 1;
+  const limit = req.query.limit || 30;
+  const offset = req.query.offset || 0;
   // *(type)* 搜索单曲(1)，歌手(100)，专辑(10)，歌单(1000)，用户(1002)
   const data = {
-    csrf_token: '',
+    csrf_token: "",
     limit,
     type,
-    s: keywords
-  }
+    s: keywords,
+    offset
+  };
 
   createWebAPIRequest(
-    'music.163.com',
-    '/weapi/search/get',
-    'POST',
+    "music.163.com",
+    "/weapi/search/get",
+    "POST",
     data,
     cookie,
     music_req => res.send(music_req),
-    err => res.status(502).send('fetch error')
-  )
-})
+    err => res.status(502).send("fetch error")
+  );
+});
 
-module.exports = router
+module.exports = router;
