7687aa1ad8e72df4ff607fd76ab5cefbdcaca741
binaryify <zhuangtongfa@qq.com>
2017-09-07 17:11:24 +0800

    修复搜索接口 offset 参数失效问题 #100

--- router/top_playlist_highquality.js
+++ router/top_playlist_highquality.js
@@ -1,25 +1,26 @@
-const express = require('express')
-const router = express()
-const { createWebAPIRequest } = require('../util/util')
+const express = require("express");
+const router = express();
+const { createWebAPIRequest } = require("../util/util");
 
-router.get('/', (req, res) => {
-  const cookie = req.get('Cookie') ? req.get('Cookie') : ''
+router.get("/", (req, res) => {
+  const cookie = req.get("Cookie") ? req.get("Cookie") : "";
   const data = {
-    cat: req.query.cat || '全部',
+    cat: req.query.cat || "全部",
     offset: req.query.offset || 0,
-    limit: req.query.limit || 20
-  }
+    limit: req.query.limit || 20,
+    csrf_token: ""
+  };
   createWebAPIRequest(
-    'music.163.com',
-    '/weapi/playlist/highquality/list',
-    'POST',
+    "music.163.com",
+    "/weapi/playlist/highquality/list",
+    "POST",
     data,
     cookie,
     music_req => {
-      res.send(music_req)
+      res.send(music_req);
     },
-    err => res.status(502).send('fetch error')
-  )
-})
+    err => res.status(502).send("fetch error")
+  );
+});
 
-module.exports = router
+module.exports = router;
