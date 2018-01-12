11805dc255bfa0b88bb5e117960f22f28c2d1c76
binaryify <zhuangtongfa@qq.com>
2018-01-12 14:47:31 +0800

    improve

--- router/user_playlist.js
+++ router/user_playlist.js
@@ -1,24 +1,27 @@
-const express = require('express')
-const router = express()
-const { createWebAPIRequest } = require('../util/util')
+const express = require("express");
+const router = express();
+const { createWebAPIRequest } = require("../util/util");
 
-router.get('/', (req, res) => {
-  const cookie = req.get('Cookie') ? req.get('Cookie') : ''
+router.get("/", (req, res) => {
+  const cookie = req.get("Cookie") ? req.get("Cookie") : "";
   const data = {
-    offset: 0,
+    offset: req.query.offset || 0,
     uid: req.query.uid,
-    limit: 1000,
-    csrf_token: ''
-  }
+    limit: req.query.limit || 30, //貌似无效
+    csrf_token: ""
+  };
   createWebAPIRequest(
-    'music.163.com',
-    '/weapi/user/playlist',
-    'POST',
+    "music.163.com",
+    "/weapi/user/playlist",
+    "POST",
     data,
     cookie,
-    music_req => res.send(music_req),
-    err => res.status(502).send('fetch error')
-  )
-})
+    music_req => {
+      res.send(music_req);
+      // console.log(JSON.parse(music_req))
+    },
+    err => res.status(502).send("fetch error")
+  );
+});
 
-module.exports = router
+module.exports = router;
