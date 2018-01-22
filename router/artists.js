79489a40df0ecfdafc68e90206b5b94e1630072d
binaryify <binaryify@gmail.com>
2018-01-22 16:44:23 +0800

    improve

--- router/artists.js
+++ router/artists.js
@@ -1,24 +1,26 @@
-const express = require('express')
-const router = express()
-const { createWebAPIRequest } = require('../util/util')
+const express = require("express");
+const router = express();
+const { createWebAPIRequest } = require("../util/util");
 
-router.get('/', (req, res) => {
-  const cookie = req.get('Cookie') ? req.get('Cookie') : ''
+router.get("/", (req, res) => {
+  const cookie = req.get("Cookie") ? req.get("Cookie") : "";
+  const id = req.query.id;
+
   const data = {
-    csrf_token: ''
-  }
-  const id = req.query.id
-  const offset = req.query.offset || 0
-  const limit = req.query.limit || 50
+    csrf_token: ""
+  };
+
   createWebAPIRequest(
-    'music.163.com',
-    `/weapi/v1/artist/${id}?offset=${offset}&limit=${limit}`,
-    'POST',
+    "music.163.com",
+    `/weapi/v1/artist/${id}`,
+    "POST",
     data,
     cookie,
-    music_req => res.send(music_req),
-    err => res.status(502).send('fetch error')
-  )
-})
+    music_req => {
+      res.send(music_req);
+    },
+    err => res.status(502).send("fetch error")
+  );
+});
 
-module.exports = router
+module.exports = router;
