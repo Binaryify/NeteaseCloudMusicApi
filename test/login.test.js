d155a1fc0177e525cb650d239b8a98a8549a85e1
binaryify <zhuangtongfa@qq.com>
2018-02-28 15:10:39 +0800

    修复登录失败会崩溃的问题

--- test/login.test.js
+++ test/login.test.js
@@ -1,71 +1,39 @@
-const assert = require('assert')
-const crypto = require('crypto')
-const { createWebAPIRequest } = require('../util/util')
+const assert = require("assert");
+const crypto = require("crypto");
+const { createWebAPIRequest } = require("../util/util");
 
-console.log('注意:测试登陆需要替换这里的账号密码!!!')
+console.log("注意:测试登陆需要替换这里的账号密码!!!");
 
-describe('测试登录是否正常', () => {
-  it('手机登录 code 应该等于200', done => {
-    const phone = '换成你的手机号'
-    const password = '换成你的密码'
-    let cookie = ''
-    const md5sum = crypto.createHash('md5')
-    md5sum.update(password)
+describe("测试登录是否正常", () => {
+  it("手机登录 code 应该等于200", done => {
+    const phone = "换成你的手机号";
+    const password = "换成你的密码";
+    let cookie = "";
+    const md5sum = crypto.createHash("md5");
+    md5sum.update(password);
     const data = {
       phone: phone,
-      password: md5sum.digest('hex'),
-      rememberLogin: 'true'
-    }
+      password: md5sum.digest("hex"),
+      rememberLogin: "true"
+    };
 
     createWebAPIRequest(
-      'music.163.com',
-      '/weapi/login/cellphone',
-      'POST',
+      "music.163.com",
+      "/weapi/login/cellphone",
+      "POST",
       data,
       cookie,
       (music_req, cookie) => {
-        const result = JSON.parse(music_req)
+        const result = JSON.parse(music_req);
         console.log({
           loginType: result.loginType,
           code: result.code,
           account: result.account
-        })
-        assert(result.code === 200)
-        done()
+        });
+        assert(result.code === 200);
+        done();
       },
       err => done(err)
-    )
-  })
-
-  it('邮箱登录 code 应该等于200', done => {
-    const email = '换成你的163网易邮箱'
-    const password = '换成你的密码'
-    const cookie = ''
-    const md5sum = crypto.createHash('md5')
-    md5sum.update(password)
-    const data = {
-      username: email,
-      password: md5sum.digest('hex'),
-      rememberLogin: 'true'
-    }
-
-    createWebAPIRequest(
-      'music.163.com',
-      '/weapi/login',
-      'POST',
-      data,
-      cookie,
-      (music_req, cookie) => {
-        const result = JSON.parse(music_req)
-        console.log({
-          loginType: result.loginType,
-          code: result.code,
-          account: result.account
-        })
-        assert(result.code === 200)
-        done()
-      },
-      err => done(err)
-    )
-  })
-})
+    );
+  });
+});
--- test/lyric.test.js
+++ test/lyric.test.js
@@ -1,22 +1,25 @@
-const assert = require('assert')
-const crypto = require('crypto')
-const { createRequest } = require('../util/util')
+const assert = require("assert");
+const crypto = require("crypto");
+const { createWebAPIRequest } = require("../util/util");
 
-describe('测试获取歌词是否正常', () => {
-  it('数据应该有 lrc 字段', done => {
-    const id = 347230
-    createRequest(
-      '/api/song/lyric?os=osx&id=' + id + '&lv=-1&kv=-1&tv=-1',
-      'GET',
-      null
-    )
-      .then(result => {
-        // console.log(JSON.parse(result).lrc)
-        assert(typeof JSON.parse(result).lrc !== 'undefined')
-        done()
-      })
-      .catch(err => {
-        done(err)
-      })
-  })
-})
+describe("测试获取歌词是否正常", () => {
+  it("数据应该有 lrc 字段", done => {
+    const cookie = "";
+    const data = {};
+    const id = 347230;
+    createWebAPIRequest(
+      "music.163.com",
+      "/weapi/song/lyric?os=osx&id=" + id + "&lv=-1&kv=-1&tv=-1",
+      "POST",
+      data,
+      cookie,
+      music_req => {
+        console.log(music_req);
+        assert(typeof JSON.parse(music_req).lrc !== "undefined");
+        done();
+        // res.send(music_req)
+      },
+      err => res.status(502).send("fetch error")
+    );
+  });
+});
