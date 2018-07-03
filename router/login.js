9d0ff70f38f10abf00278652c73ec9b106360da7
binaryify <zhuangtongfa.com>
2018-07-03 09:11:09 +0800

    修复无法使用邮箱问题

--- router/login.js
+++ router/login.js
@@ -1,35 +1,32 @@
-const crypto = require("crypto");
+const crypto = require('crypto')
 
 //邮箱登录
 module.exports = (req, res, createWebAPIRequest, request) => {
-  const email = req.query.email;
-  const cookie = req.get("Cookie") ? req.get("Cookie") : "";
-  const md5sum = crypto.createHash("md5");
-  md5sum.update(req.query.password);
+  const email = req.query.email
+  const cookie = req.get('Cookie') ? req.get('Cookie') : ''
+  const md5sum = crypto.createHash('md5')
+  md5sum.update(req.query.password)
   const data = {
     username: email,
-    password: md5sum.digest("hex"),
-    rememberLogin: "true",
-    clientToken:
-      "1_jVUMqWEPke0/1/Vu56xCmJpo5vP1grjn_SOVVDzOc78w8OKLVZ2JH7IfkjSXqgfmh"
-  };
-  console.log(email, req.query.password);
+    password: md5sum.digest('hex'),
+    rememberLogin: 'true'
+  }
+  console.log(email, req.query.password)
 
   createWebAPIRequest(
-    "music.163.com",
-    "/weapi/login?csrf_token=",
-    "POST",
+    'music.163.com',
+    '/weapi/login',
+    'POST',
     data,
     cookie,
     (music_req, cookie) => {
       // console.log(music_req)
-      cookie =
-        cookie && cookie.map(x => x.replace("Domain=.music.163.com", ""));
+      cookie = cookie && cookie.map(x => x.replace('Domain=.music.163.com', ''))
       res.set({
-        "Set-Cookie": cookie
-      });
-      res.send(music_req);
+        'Set-Cookie': cookie
+      })
+      res.send(music_req)
     },
-    err => res.status(502).send("fetch error")
-  );
-};
+    err => res.status(502).send('fetch error')
+  )
+}
