94b2f755bf89fcaa2c4c6f98063a5a3fd370b3b7
Nzix <iminezn5656@outlook.com>
2018-08-29 21:08:03 +0800

    fix login status query bug

--- router/login_status.js
+++ router/login_status.js
@@ -8,12 +8,16 @@ module.exports = (req, res, createWebAPIRequest, request) => {
     {},
     cookie,
     (music_req, cookie) => {
-      var userInfo = (/var GUser=([^;]+);/g).exec(music_req)[1];
-      var bindInfo = (/var GBinds=([^;]+);/g).exec(music_req)[1];
-      userInfo = eval(`(${userInfo})`);
-      userInfo.userBind = eval(`(${bindInfo})`);
-      userInfo.userBind.forEach((item) => {item.tokenJsonStr = JSON.parse(item.tokenJsonStr)});
-      res.send(userInfo);
+      try {
+        var userInfo = (/var GUser=([^;]+);/g).exec(music_req)[1];
+        var bindInfo = (/var GBinds=([^;]+);/g).exec(music_req)[1];
+        userInfo = eval(`(${userInfo})`);
+        userInfo.userBind = eval(`(${bindInfo})`);
+        userInfo.userBind.forEach((item) => {item.tokenJsonStr = JSON.parse(item.tokenJsonStr)});
+        res.send(userInfo);
+      } catch (error) {
+        res.status(502).send("fetch error");
+      }
     },
     err => res.status(502).send("fetch error")
   );
