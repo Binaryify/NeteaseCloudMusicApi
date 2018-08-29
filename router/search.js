4fd1effcb078c4d70e8a70de0e0274fe59590879
binaryify <zhuangtongfa.com>
2018-08-29 11:00:13 +0800

    新增获取视频数据接口,完善文档 #301

--- router/search.js
+++ router/search.js
@@ -1,26 +1,26 @@
 // 搜索
 module.exports = (req, res, createWebAPIRequest, request) => {
-  const cookie = req.get("Cookie") ? req.get("Cookie") : "";
-  const keywords = req.query.keywords;
-  const type = req.query.type || 1;
-  const limit = req.query.limit || 30;
-  const offset = req.query.offset || 0;
-  // *(type)* 搜索单曲(1)，歌手(100)，专辑(10)，歌单(1000)，用户(1002)
+  const cookie = req.get('Cookie') ? req.get('Cookie') : ''
+  const keywords = req.query.keywords
+  const type = req.query.type || 1
+  const limit = req.query.limit || 30
+  const offset = req.query.offset || 0
+  // *(type)* 搜索类型；默认为 1 即单曲 , 取值意义 : 1: 单曲, 10: 专辑, 100: 歌手, 1000: 歌单, 1002: 用户, 1004: MV, 1006: 歌词, 1009: 电台, 1014: 视频
   const data = {
-    csrf_token: "",
+    csrf_token: '',
     limit,
     type,
     s: keywords,
     offset
-  };
+  }
 
   createWebAPIRequest(
-    "music.163.com",
-    "/weapi/search/get",
-    "POST",
+    'music.163.com',
+    '/weapi/search/get',
+    'POST',
     data,
     cookie,
     music_req => res.send(music_req),
-    err => res.status(502).send("fetch error")
-  );
-};
+    err => res.status(502).send('fetch error')
+  )
+}
