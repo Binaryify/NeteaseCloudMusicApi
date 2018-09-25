b8c9f97757749d4334b72199c8ea3fc1a1360fb0
Nzix <iminezn5656@outlook.com>
2018-09-25 19:38:10 +0800

    modify regular expression

--- router/login_status.js
+++ router/login_status.js
@@ -7,10 +7,10 @@ module.exports = (req, res, createWebAPIRequest, request) => {
     "GET",
     {},
     cookie,
-    (music_req, cookie) => {
+    music_req => {
       try {
-        var profile = (/var GUser=([^;]+);/g).exec(music_req)[1];
-        var bindings = (/var GBinds=([^;]+);/g).exec(music_req)[1];
+        var profile = /GUser\s*=\s*([^;]+);/.exec(music_req)[1];
+        var bindings = /GBinds\s*=\s*([^;]+);/.exec(music_req)[1];
         profile = eval(`(${profile})`);
         bindings = eval(`(${bindings})`);
         res.send({code: 200, profile: profile, bindings: bindings});
