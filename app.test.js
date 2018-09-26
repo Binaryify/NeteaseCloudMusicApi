1bef762d74e01e86c4ffc7ac7d545940f285b5fd
Nzix <iminezn5656@outlook.com>
2018-09-26 14:19:52 +0800

    correct some mistakes for global variable

--- app.test.js
+++ app.test.js
@@ -1,16 +1,18 @@
 const fs = require('fs')
 const path = require('path')
+const request = require('request')
 
 let app
 before(() => {
   app = require('./app.js')
   global.host = 'http://localhost:' + app.server.address().port
+  request.debug = false
 })
 after((done) => {
   app.server.close(done)
 })
 
-fs.readdirSync(path.resolve(__dirname, 'test'))
+fs.readdirSync(path.join(__dirname, 'test'))
 .forEach(file => {
-    require('./test/' + file)
+    require(path.join(__dirname, 'test', file))
 })
