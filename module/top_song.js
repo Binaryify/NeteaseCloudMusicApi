696e4b46be657283679a6cc1877f50a8bdba6368
Nzix <iminezn5656@outlook.com>
2018-10-20 13:17:58 +0800

    internal modify

--- module/top_song.js
+++ module/top_song.js
@@ -1,8 +1,14 @@
-// 最新单曲(暂时废弃?)
+// 新歌速递
 
 module.exports = (query, request) => {
+    const data = {
+        areaId: query.type || 0, // 全部:0 华语:7 欧美:96 日本:8 韩国:16
+        limit: query.limit || 100,
+        offset: query.offset || 0,
+        total: true
+    }
     return request(
-        'POST', `http://music.163.com/weapi/v1/discovery/new/songs`, {},
+        'POST', `http://music.163.com/weapi/v1/discovery/new/songs`, data,
         {crypto: 'weapi', cookie: query.cookie, proxy: query.proxy}
     )
 }
