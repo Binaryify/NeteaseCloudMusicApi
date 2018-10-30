389757d7852672c29884a60590fe5bbab0070793
Nzix <iminezn5656@outlook.com>
2018-10-31 00:29:58 +0800

    POST method support fixed

--- module/top_song.js
+++ module/top_song.js
@@ -1,16 +1,14 @@
 // 新歌速递
 
 module.exports = (query, request) => {
-  const data = {
-    areaId: query.type || 0, // 全部:0 华语:7 欧美:96 日本:8 韩国:16
-    limit: query.limit || 100,
-    offset: query.offset || 0,
-    total: true
-  }
-  return request(
-    'POST',
-    `http://music.163.com/weapi/v1/discovery/new/songs`,
-    data,
-    { crypto: 'weapi', cookie: query.cookie, proxy: query.proxy }
-  )
+    const data = {
+        areaId: query.type || 0, // 全部:0 华语:7 欧美:96 日本:8 韩国:16
+        limit: query.limit || 100,
+        offset: query.offset || 0,
+        total: true
+    }
+    return request(
+        'POST', `http://music.163.com/weapi/v1/discovery/new/songs`, data,
+        {crypto: 'weapi', cookie: query.cookie, proxy: query.proxy}
+    )
 }
