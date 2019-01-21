c453c1dc95684850bbef59489f51d06a4d92cbd6
Liu Enze <liuenze6516@gmail.com>
2019-01-21 14:13:03 +0800

    增加videogroup的获取

---
+++ module/video_group.js
@@ -0,0 +1,18 @@
+// 视频链接
+
+module.exports = (query, request) => {
+  const data = {
+    groupId: query.id,
+    name: query.name,
+    offset: 0,
+    needUrl: true,
+    resolution: query.res || 1080
+  }
+  return request(
+    'POST', `https://music.163.com/weapi/videotimeline/videogroup/get`, data, {
+      crypto: 'weapi',
+      cookie: query.cookie,
+      proxy: query.proxy
+    }
+  )
+}
