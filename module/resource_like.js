a61c1250fcb5ef1809ab1ef8238f2bfd11a374c1
binaryify <binaryify@gmail.com>
2019-03-14 16:03:56 +0800

    v3.5.0 增加获取动态评论接口,增加点赞和评论对动态的支持

--- module/resource_like.js
+++ module/resource_like.js
@@ -1,14 +1,19 @@
 // 点赞与取消点赞资源
 
 module.exports = (query, request) => {
+    query.cookie.os = 'pc'
     query.t = (query.t == 1 ? 'like' : 'unlike')
     query.type = {
         1: 'R_MV_5_', //  MV
         4: 'A_DJ_1_', //  电台
-        5: 'R_VI_62_' //  视频
+        5: 'R_VI_62_', //  视频
+        6: 'A_EV_2_'
     }[query.type]
     const data = {
         threadId: query.type + query.id
+    }
+    if(query.type=='A_EV_2_'){
+        data.threadId=query.threadId
     }
     return request(
         'POST', `https://music.163.com/weapi/resource/${query.t}`, data,
