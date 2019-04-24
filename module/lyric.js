a260e3b352fb9793b01a1415d677224b66a2abfd
binaryify <binaryify@gmail.com>
2019-04-24 19:29:48 +0800

    修复歌词接口出错问题

--- module/lyric.js
+++ module/lyric.js
@@ -1,8 +1,11 @@
 // 歌词
 
 module.exports = (query, request) => {
+    const data={
+        id:query.id
+    }
     return request(
-        'POST', `https://music.163.com/weapi/song/lyric?os=osx&id=${query.id}&lv=-1&kv=-1&tv=-1`, {},
-        {crypto: 'weapi', cookie: query.cookie, proxy: query.proxy}
+        'POST', `https://music.163.com/weapi/song/lyric?lv=-1&kv=-1&tv=-1`, data,
+        {crypto: 'linuxapi', cookie: query.cookie, proxy: query.proxy}
     )
 }
