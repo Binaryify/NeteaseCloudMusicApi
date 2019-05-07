6b0478da1a08dbac05ada72a710c8002cb30455a
jiakang <1079153785@qq.com>
2019-05-07 21:47:21 +0800

    增加转发动态接口

---
+++ module/forward.js
@@ -0,0 +1,17 @@
+// ×ª·¢¶¯Ì¬
+
+const crypto = require('crypto')
+
+module.exports = (query, request) => {
+    query.cookie.os = 'osx'
+    const data = {
+		forwards: query.forwards,
+		id: query.id,
+        eventUserId: query.eventUserId,
+		checkToken:query.checkToken
+    }
+    return request(
+        'POST', `https://music.163.com/weapi/event/forward`, data,
+        {crypto: 'weapi', ua: 'pc', cookie: query.cookie, proxy: query.proxy}
+    )
+}
