e7df5cd70abc011782f367f020c7a20dc2944b2a
binaryify <binaryify@gmail.com>
2019-05-10 16:05:09 +0800

    v3.12.0

--- module/playlist_create.js
+++ module/playlist_create.js
@@ -1,12 +1,14 @@
 // 创建歌单
 
 module.exports = (query, request) => {
-    query.cookie.os = 'pc'
-    const data = {
-        name: query.name
-    }
-    return request(
-        'POST', `https://music.163.com/weapi/playlist/create`, data,
-        {crypto: 'weapi', cookie: query.cookie, proxy: query.proxy}
-    )
-}
+  query.cookie.os = "pc";
+  const data = {
+    name: query.name,
+    privacy: query.privacy    //0 为普通歌单，10 为隐私歌单
+  };
+  return request("POST", `https://music.163.com/weapi/playlist/create`, data, {
+    crypto: "weapi",
+    cookie: query.cookie,
+    proxy: query.proxy
+  });
+};
