e92e8029e1b8721f5e16d1579aaf72028774fd41
lizhenyuls <1142469379@qq.com>
2019-05-16 23:01:27 +0800

    新增eapi算法

---
+++ module/batch.js
@@ -0,0 +1,14 @@
+module.exports = (query, request) => {
+  const data = {
+    "e_r": true
+  };
+  Object.keys(query).forEach(i => {
+    if (/^\/api\//.test(i)) {
+      data[i] = query[i]
+    }
+  })
+  return request(
+    'POST', `http://music.163.com/eapi/batch`, data,
+    {crypto: 'eapi', proxy: query.proxy, url: '/api/batch', cookie: query.cookie}
+  )
+};
