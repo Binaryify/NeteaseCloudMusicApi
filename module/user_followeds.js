b20e9d7733aef03450f086c068748afb333210cb
binaryify <binaryify@gmail.com>
2019-06-09 19:08:25 +0800

    修复获取用户粉丝列表接口只能获取1000个的问题

--- module/user_followeds.js
+++ module/user_followeds.js
@@ -1,13 +1,20 @@
 // 关注TA的人(粉丝)
 
 module.exports = (query, request) => {
-    const data = {
-        userId: query.uid,
-        limit: query.limit || 30,
-        offset: query.offset || 0
+  const data = {
+    userId: query.uid,
+    limit: query.limit || 30,
+    offset: query.offset || 0
+  };
+  return request(
+    "POST",
+    `https://music.163.com/eapi/user/getfolloweds/${query.uid}`,
+    data,
+    {
+      crypto: "eapi",
+      cookie: query.cookie,
+      proxy: query.proxy,
+      url: "/api/user/getfolloweds"
     }
-    return request(
-        'POST', `https://music.163.com/weapi/user/getfolloweds`, data,
-        {crypto: 'weapi', cookie: query.cookie, proxy: query.proxy}
-    )
-}
+  );
+};
