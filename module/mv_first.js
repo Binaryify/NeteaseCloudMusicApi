d4eab6d1bf01a37d4969374bd7d012c3ce709e7a
binaryify <zhuangtongfa@7daichina.com>
2019-06-16 22:08:04 +0800

    v3.15.0 新增`获取视频标签列表`,`网易出品`,`全部mv`接口, `最新 mv` 和 `mv 排行` 接口加入地区参 数,更新文档 #505

--- module/mv_first.js
+++ module/mv_first.js
@@ -1,13 +1,15 @@
 // 最新MV
 
 module.exports = (query, request) => {
-    const data = {
-        // 'offset': query.offset || 0,
-        limit: query.limit || 30,
-        total: true
-    }
-    return request(
-        'POST', `https://music.163.com/weapi/mv/first`, data,
-        {crypto: 'weapi', cookie: query.cookie, proxy: query.proxy}
-    )
-}
+  const data = {
+    // 'offset': query.offset || 0,
+    area: query.area || "",
+    limit: query.limit || 30,
+    total: true
+  };
+  return request("POST", `https://interface.music.163.com/weapi/mv/first`, data, {
+    crypto: "weapi",
+    cookie: query.cookie,
+    proxy: query.proxy
+  });
+};
--- module/top_mv.js
+++ module/top_mv.js
@@ -1,13 +1,15 @@
 // MV排行榜
 
 module.exports = (query, request) => {
-    const data = {
-        limit: query.limit || 30,
-        offset: query.offset || 0,
-        total: true
-    }
-    return request(
-        'POST', `https://music.163.com/weapi/mv/toplist`, data,
-        {crypto: 'weapi', cookie: query.cookie, proxy: query.proxy}
-    )
-}
+  const data = {
+    area: query.area || "",
+    limit: query.limit || 30,
+    offset: query.offset || 0,
+    total: true
+  };
+  return request("POST", `https://music.163.com/weapi/mv/toplist`, data, {
+    crypto: "weapi",
+    cookie: query.cookie,
+    proxy: query.proxy
+  });
+};
