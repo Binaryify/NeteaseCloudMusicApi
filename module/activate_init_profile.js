df1f02b86a672db72477aa0b4e1439a2f7820a14
kurodaakira-jp <kurodaakira.jp@gmail.com>
2019-07-23 17:49:55 +0800

    验证手机是否注册、验证用户名是否注册

---
+++ module/activate_init_profile.js
@@ -0,0 +1,18 @@
+// 检测是否注册
+
+module.exports = (query, request) => {
+  const data = {
+    nickname: query.nickname
+  };
+  return request(
+    "POST",
+    `http://music.163.com/eapi/activate/initProfile`,
+    data,
+    {
+      crypto: "eapi",
+      cookie: query.cookie,
+      proxy: query.proxy,
+      url: '/api/activate/initProfile'
+    }
+  );
+};
