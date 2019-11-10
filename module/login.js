9443bb25625a68d1e79ab25b24227393ddecd836
binaryify <binaryify@gmail.com>
2019-11-10 11:47:03 +0800

    去除多余代码

--- module/login.js
+++ module/login.js
@@ -11,19 +11,17 @@ module.exports = async (query, request) => {
   }
   const result = await request(
     'POST', `https://music.163.com/weapi/login`, data,
-    {crypto: 'weapi', ua: 'pc', cookie: query.cookie, proxy: query.proxy}
+    { crypto: 'weapi', ua: 'pc', cookie: query.cookie, proxy: query.proxy }
   )
-  if(result.body.code === 502){
-    return new Promise(resolve => {
-      resolve({
-        status: 200,
-        body: {
-          'msg': '账号或密码错误',
-          'code': 502,
-          'message': '账号或密码错误'
-        }
-      })
-    })
+  if (result.body.code === 502) {
+    return {
+      status: 200,
+      body: {
+        'msg': '账号或密码错误',
+        'code': 502,
+        'message': '账号或密码错误'
+      }
+    }
   }
   return result
 }
