afd468ca183998232cbf440cfaf2462c10d57e4c
binaryify <binaryify@gmail.com>
2020-06-23 14:47:43 +0800

    更新每日推荐接口 #826

--- module/recommend_songs.js
+++ module/recommend_songs.js
@@ -1,11 +1,11 @@
 // 每日推荐歌曲
 
 module.exports = (query, request) => {
+  query.cookie.os = 'ios'
   const data = {
-    total: true
   }
   return request(
-    'POST', `https://music.163.com/weapi/v1/discovery/recommend/songs`, data,
+    'POST', `https://music.163.com/api/v3/discovery/recommend/songs`, data,
     {crypto: 'weapi', cookie: query.cookie, proxy: query.proxy}
   )
 }
