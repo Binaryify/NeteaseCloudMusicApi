885d1930f2bd351576817e9c19102c62f0a270e8
binaryify <binaryify@gmail.com>
2020-06-25 17:55:27 +0800

    排行榜接口废弃idx参数,只支持id参数,修复返回数据异常问题 #830 ,新增获取历史日推可用日期列表,获取历史日推详细数据接口

---
+++ module/history_recommend_songs_detail.js
@@ -0,0 +1,12 @@
+// 历史每日推荐歌曲
+
+module.exports = (query, request) => {
+  query.cookie.os = 'ios'
+  const data = {
+    date: query.date || '',
+  }
+  return request(
+    'POST', `https://music.163.com/api/discovery/recommend/songs/history/detail`, data,
+    {crypto: 'weapi', cookie: query.cookie, proxy: query.proxy}
+  )
+}
