0ad746dad41277b6d9cfb44f941c1b265e34f3d2
binaryify <binaryify@gmail.com>
2020-07-26 16:45:56 +0800

    新增`全部新碟`,`数字专辑-新碟上架`,`数字专辑&数字单曲-榜单`,`数字专辑-语种风格馆`,`数字专辑详情`接口,更新`新碟上架`接口,修改传入参数,返回数据结构有变化

--- module/top_album.js
+++ module/top_album.js
@@ -1,14 +1,18 @@
 // 新碟上架
 
 module.exports = (query, request) => {
+  const date = new Date()
+
   const data = {
-    area: query.type || 'ALL', // ALL,ZH,EA,KR,JP
+    area: query.area || 'ALL', // //ALL:全部,ZH:华语,EA:欧美,KR:韩国,JP:日本
     limit: query.limit || 50,
     offset: query.offset || 0,
-    total: true
+    type: query.type || 'new',
+    year: query.year || date.getFullYear(),
+    month: query.month || date.getMonth() + 1,
   }
   return request(
-    'POST', `https://music.163.com/weapi/album/new`, data,
+    'POST', `https://music.163.com/api/discovery/new/albums/area`, data,
     {crypto: 'weapi', cookie: query.cookie, proxy: query.proxy}
   )
 }
