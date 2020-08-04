3c638e5f0af73587e8294ae457b2e52f207956c5
a632079 <i@a632079.me>
2020-08-05 02:09:13 +0800

    feat: optional data in weblog api

--- module/weblog.js
+++ module/weblog.js
@@ -4,7 +4,7 @@ module.exports = (query, request) => {
   return request(
     'POST',
     `https://music.163.com/weapi/feedback/weblog`,
-    {},
+    query.data || {},
     {
       crypto: 'weapi',
       cookie: query.cookie,
