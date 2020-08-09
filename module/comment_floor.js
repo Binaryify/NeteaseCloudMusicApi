324cd8b67e612b6d388cbf51b6b991df0455fe23
binaryify <binaryify@gmail.com>
2020-08-09 17:37:55 +0800

    新增歌曲楼层评论,歌手全部歌曲接口 #864, #867, 支持收藏VIP或付费歌曲到歌单 #860, 支持手动传入realIP #863

---
+++ module/comment_floor.js
@@ -0,0 +1,27 @@
+module.exports = (query, request) => {
+  query.type = {
+    0: 'R_SO_4_', //  歌曲
+    1: 'R_MV_5_', //  MV
+    2: 'A_PL_0_', //  歌单
+    3: 'R_AL_3_', //  专辑
+    4: 'A_DJ_1_', //  电台,
+    5: 'R_VI_62_', //  视频
+  }[query.type]
+  const data = {
+    parentCommentId: query.parentCommentId,
+    threadId: query.type + query.id,
+    time: query.time || -1,
+    limit: query.limit || 20,
+  }
+  return request(
+    'POST',
+    `https://music.163.com/api/resource/comment/floor/get`,
+    data,
+    {
+      crypto: 'weapi',
+      cookie: query.cookie,
+      proxy: query.proxy,
+      realIP: query.realIP,
+    }
+  )
+}
