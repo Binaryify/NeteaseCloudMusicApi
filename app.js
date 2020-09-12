3b15fe9024a65e2756efa4e575098865b4b178cc
binaryify <binaryify@gmail.com>
2020-09-12 10:23:21 +0800

    修复代码错误

--- app.js
+++ app.js
@@ -38,15 +38,14 @@ app.use((req, res, next) => {
 
 // cookie parser
 app.use((req, res, next) => {
-  req.cookies = {}(req.headers.cookie || '')
-    .split(/\s*;\s*/)
-    .forEach((pair) => {
-      let crack = pair.indexOf('=')
-      if (crack < 1 || crack == pair.length - 1) return
-      req.cookies[
-        decodeURIComponent(pair.slice(0, crack)).trim()
-      ] = decodeURIComponent(pair.slice(crack + 1)).trim()
-    })
+  req.cookies = {}
+  ;(req.headers.cookie || '').split(/\s*;\s*/).forEach((pair) => {
+    let crack = pair.indexOf('=')
+    if (crack < 1 || crack == pair.length - 1) return
+    req.cookies[
+      decodeURIComponent(pair.slice(0, crack)).trim()
+    ] = decodeURIComponent(pair.slice(crack + 1)).trim()
+  })
   next()
 })
 
