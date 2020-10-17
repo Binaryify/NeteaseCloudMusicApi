ed2e5fa953d2fb564e492d555799a70f147ccc33
binaryify <binaryify@gmail.com>
2020-10-17 14:02:42 +0800

    更新电台详情/电台节目详情接口 #977

--- module/dj_detail.js
+++ module/dj_detail.js
@@ -4,7 +4,7 @@ module.exports = (query, request) => {
   const data = {
     id: query.rid,
   }
-  return request('POST', `https://music.163.com/weapi/djradio/get`, data, {
+  return request('POST', `https://music.163.com/api/djradio/v2/get`, data, {
     crypto: 'weapi',
     cookie: query.cookie,
     proxy: query.proxy,
--- module/dj_program_detail.js
+++ module/dj_program_detail.js
@@ -4,15 +4,10 @@ module.exports = (query, request) => {
   const data = {
     id: query.id,
   }
-  return request(
-    'POST',
-    `https://music.163.com/weapi/dj/program/detail`,
-    data,
-    {
-      crypto: 'weapi',
-      cookie: query.cookie,
-      proxy: query.proxy,
-      realIP: query.realIP,
-    },
-  )
+  return request('POST', `https://music.163.com/api/dj/program/detail`, data, {
+    crypto: 'weapi',
+    cookie: query.cookie,
+    proxy: query.proxy,
+    realIP: query.realIP,
+  })
 }
