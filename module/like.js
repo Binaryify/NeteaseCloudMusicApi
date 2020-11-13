00b63cfa7ad97865548ae471052911b01662fc1f
binaryify <binaryify@gmail.com>
2020-11-13 10:50:28 +0800

    修复使用post请求取消喜欢音乐会失败的问题 #1024

--- module/like.js
+++ module/like.js
@@ -2,11 +2,12 @@
 const { toBoolean } = require('../util')
 
 module.exports = (query, request) => {
-  query.like = query.like == 'false' ? false : true
+  query.like = toBoolean(query.like)
   const data = {
     trackId: query.id,
     like: query.like,
   }
+  console.log(query.like)
   return request(
     'POST',
     `https://music.163.com/weapi/radio/like?alg=${
