4aecf1575fcf8123a03711de1477a9574f799743
许富荧 <xufuying@xufuyingdeMacBook-Pro.local>
2022-01-24 23:23:03 +0800

    update

--- app.js
+++ app.js
@@ -136,7 +136,7 @@ fs.readdirSync(path.join(__dirname, 'module'))
     })
   })
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 9000
 const host = process.env.HOST || ''
 
 app.server = app.listen(port, host, () => {
