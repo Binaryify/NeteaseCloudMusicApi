b02d4d5e046179aac8608246d3569f3e0f42b90b
pan93412 <pan93412@gmail.com>
2022-01-28 12:52:21 +0800

    refactor: allow using server statically
    將原本 "NeteaseCloudMusicApi/server"
    掃描 modules 目錄的部分抽出 server.js，
    改移到 main.js。這樣使用者就有辦法只載入
    靜態的伺服器部分（不動態載入模組）。
    
    這個 patch 的 breaking changes 不易發生。
    可作為 patch version 發佈。

--- server.js
+++ server.js
@@ -294,28 +294,7 @@ async function serveNcmApi(options) {
   return appExt
 }
 
-let obj = {}
-fs.readdirSync(path.join(__dirname, 'module'))
-  .reverse()
-  .forEach((file) => {
-    if (!file.endsWith('.js')) return
-    let fileModule = require(path.join(__dirname, 'module', file))
-    obj[file.split('.').shift()] = function (data) {
-      if (typeof data.cookie === 'string') {
-        data.cookie = cookieToJson(data.cookie)
-      }
-      return fileModule(
-        {
-          ...data,
-          cookie: data.cookie ? data.cookie : {},
-        },
-        request,
-      )
-    }
-  })
-
 module.exports = {
   serveNcmApi,
   getModulesDefinitions,
-  ...obj,
 }
