d5250e859050e0c277c07749adab86b3bbfb0591
Aki Nazuki <43605695+akinazuki@users.noreply.github.com>
2022-05-06 09:16:13 +0800

    增加支持罗马音歌词返回

--- module/lyric.js
+++ module/lyric.js
@@ -1,17 +1,24 @@
 // 歌词
 
 module.exports = (query, request) => {
-  query.cookie.os = 'pc'
+  query.cookie.os = 'ios'
+
   const data = {
     id: query.id,
-    lv: -1,
-    kv: -1,
-    tv: -1,
+    tv: '5',
+    lv: '4',
+    rv: '4',
+    kv: '4',
   }
-  return request('POST', `https://music.163.com/api/song/lyric`, data, {
-    crypto: 'api',
-    cookie: query.cookie,
-    proxy: query.proxy,
-    realIP: query.realIP,
-  })
+  return request(
+    'POST',
+    `https://music.163.com/api/song/lyric?_nmclfl=1`,
+    data,
+    {
+      crypto: 'api',
+      cookie: query.cookie,
+      proxy: query.proxy,
+      realIP: query.realIP,
+    },
+  )
 }
