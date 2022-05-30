47bd9175ee66ef4cc7e865246735555165e623d2
binaryify <binaryify@gmail.com>
2022-05-30 09:46:38 +0800

    修复测试不通过的问题

--- test/search.test.js
+++ test/search.test.js
@@ -1,12 +1,13 @@
 const assert = require('assert')
 const axios = require('axios')
 const host = global.host || 'http://localhost:3000'
-
+const config = require('../util/config.json')
 describe('测试搜索是否正常', () => {
   it('获取到的数据的 name 应该和搜索关键词一致', (done) => {
     const qs = {
       keywords: '海阔天空',
       type: 1,
+      cookie: config.anonymous_token,
     }
     axios
       .get(`${host}/search`, {
