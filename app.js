c2621a58705a4f8552a6b1dfc1a2e0daa9b1e943
binaryify <binaryify@gmail.com>
2023-08-21 21:34:41 +0800

    update: anonymous_token 动态生成

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
+const generateConfig = require('./generateConfig')
 async function start() {
+  await generateConfig()
   require('./server').serveNcmApi({
     checkVersion: true,
   })
