35eff2d5106e8549dd5fb591209c8c1226c82eb0
XiangZi7 <793923048@qq.com>
2023-08-24 10:07:15 +0800

    动漫接口

---
+++ module/video_chapter.js
@@ -0,0 +1,13 @@
+// 动漫-搜索
+module.exports = (query, request) => {
+    return request(
+      'get',
+      `http://api.pingcc.cn/videoChapter/search/${query.id}`,
+      {
+        crypto: 'weapi',
+        cookie: query.cookie,
+        proxy: query.proxy,
+        realIP: query.realIP,
+      },
+    )
+  }
---
+++ module/video_search.js
@@ -0,0 +1,17 @@
+module.exports = (query, request) => {
+    const data = {
+      name: query.name,
+      pageNum: query.pageNum || 1,
+      pageSize: query.pageNum || 10,
+    }
+    return request(
+      'get',
+      `http://api.pingcc.cn/video/search/title/${data.name}/${data.pageNum}/${data.pageSize}`,
+      // {
+      //     crypto: 'weapi',
+      //     cookie: query.cookie,
+      //     proxy: query.proxy,
+      //     realIP: query.realIP,
+      // },
+    )
+  }
