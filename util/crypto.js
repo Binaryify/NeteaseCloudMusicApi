edb8295a7f477c692e8e0f91aa7ff8ea210473f4
groovejames <groovejames@users.noreply.github.com>
2023-09-14 19:02:55 +0200

    compatibility with Deno
    - public key now with 64 chars line length
    - replacement for crypto.publicEncrypt(..., RSA_NO_PADDING)
      which is not available in Deno
    - needs new dependency bigint-mod-arith for the modPow() function
    - minimum required Node.js version is now 16.x because usage of
      crypto.createPublicKey(key).export({ format: 'jwk' })
      (could also be solved differently if you want to stick with 12.x)

--- util/crypto.js
+++ util/crypto.js
@@ -1,10 +1,16 @@
 const crypto = require('crypto')
+const { modPow } = require('bigint-mod-arith')
 const iv = Buffer.from('0102030405060708')
 const presetKey = Buffer.from('0CoJUm6Qyw8W8jud')
 const linuxapiKey = Buffer.from('rFgB&h#%2?^eDg:Q')
 const base62 = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
 const publicKey =
-  '-----BEGIN PUBLIC KEY-----\nMIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDgtQn2JZ34ZC28NWYpAUd98iZ37BUrX/aKzmFbt7clFSs6sXqHauqKWqdtLkF2KexO40H1YTX8z2lSgBBOAxLsvaklV8k4cBFK9snQXE9/DDaFt6Rr7iVZMldczhC0JNgTz+SHXT6CBHuX3e9SdB1Ua44oncaTWz7OBGLbCiK45wIDAQAB\n-----END PUBLIC KEY-----'
+  '-----BEGIN PUBLIC KEY-----\n' +
+  'MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDgtQn2JZ34ZC28NWYpAUd98iZ3\n' +
+  '7BUrX/aKzmFbt7clFSs6sXqHauqKWqdtLkF2KexO40H1YTX8z2lSgBBOAxLsvakl\n' +
+  'V8k4cBFK9snQXE9/DDaFt6Rr7iVZMldczhC0JNgTz+SHXT6CBHuX3e9SdB1Ua44o\n' +
+  'ncaTWz7OBGLbCiK45wIDAQAB\n' +
+  '-----END PUBLIC KEY-----'
 const eapiKey = 'e82ckenh8dichen8'
 
 const aesEncrypt = (buffer, mode, key, iv) => {
@@ -13,18 +19,28 @@ const aesEncrypt = (buffer, mode, key, iv) => {
 }
 
 const rsaEncrypt = (buffer, key) => {
+  const base = BigInt('0x' + Buffer.from(buffer).toString('hex'))
+  const pubKey = crypto.createPublicKey(key).export({ format: 'jwk' })
+  const modulus = BigInt('0x' + Buffer.from(pubKey.n, 'base64').toString('hex'))
+  const exponent = BigInt(
+    '0x' + Buffer.from(pubKey.e, 'base64').toString('hex'),
+  )
+  const result = modPow(base, exponent, modulus).toString(16)
+  return result.padStart(256, '0')
+  /* original, using crypto.publicEncrypt() with RSA_NO_PADDING:
   buffer = Buffer.concat([Buffer.alloc(128 - buffer.length), buffer])
   return crypto.publicEncrypt(
     { key: key, padding: crypto.constants.RSA_NO_PADDING },
     buffer,
-  )
+  ).toString('hex')
+  */
 }
 
 const weapi = (object) => {
   const text = JSON.stringify(object)
   const secretKey = crypto
     .randomBytes(16)
-    .map((n) => base62.charAt(n % 62).charCodeAt())
+    .map((n) => base62.charAt(n % 62).charCodeAt(0))
   return {
     params: aesEncrypt(
       Buffer.from(
@@ -34,7 +50,7 @@ const weapi = (object) => {
       secretKey,
       iv,
     ).toString('base64'),
-    encSecKey: rsaEncrypt(secretKey.reverse(), publicKey).toString('hex'),
+    encSecKey: rsaEncrypt(secretKey.reverse(), publicKey),
   }
 }
 
