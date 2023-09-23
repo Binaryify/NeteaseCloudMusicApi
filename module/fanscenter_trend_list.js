5506e31c433f8191ed5e649db06559ffc5a8b42e
overwriter <no@email.address>
2023-09-23 11:01:31 +0800

    fix:修复粉丝来源接口

--- module/fanscenter_trend_list.js
+++ module/fanscenter_trend_list.js
@@ -1,7 +1,11 @@
 // 粉丝来源
 const crypto = require('crypto')
 module.exports = (query, request) => {
-  const data = {}
+  const data = {
+    startTime: query.startTime || Date.now() - 7 * 24 * 3600 * 1000,
+    endTime: query.endTime || Date.now(),
+    type: query.type || 0, //新增关注:0 新增取关:1
+  }
   return request(
     'POST',
     `https://interface.music.163.com/weapi/fanscenter/trend/list`,
