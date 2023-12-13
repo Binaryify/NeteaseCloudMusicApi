fa5c733895bfe6cbf55380c2d7072f367bf6a09b
binaryify <binaryify@gmail.com>
2023-12-13 13:51:38 +0800

    fix: /artist/detail  登录状态下调用提示网络拥挤的问题 #1853

--- module/artist_detail.js
+++ module/artist_detail.js
@@ -1,4 +1,6 @@
 module.exports = (query, request) => {
+  query.cookie.os = 'ios'
+  query.cookie.appver = '8.20.21'
   return request(
     'POST',
     `https://music.163.com/api/artist/head/info/get`,
